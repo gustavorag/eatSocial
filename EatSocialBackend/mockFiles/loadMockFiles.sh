a4ce4c1caf1d256d1ed9d0be418004d10307a3f8
Gustao <gustavorag@gmail.com>
2018-02-17 16:43:02 -0800

    changing mock files

--- EatSocialBackend/mockFiles/loadMockFiles.sh
+++ EatSocialBackend/mockFiles/loadMockFiles.sh
@@ -1,14 +1,14 @@
 echo "Creating database"
 mongo localhost:27017/admin --eval "use eatSocialDev;"
 echo "Loading restaurants mock data"
-mongoimport --db eatSocialDev --collection restaurants --drop --host=127.0.0.1 --file restaurants.json
+mongoimport --db eatSocialDev --collection restaurants --drop --host=127.0.0.1 --file restaurants.json --jsonArray
 echo "Restaurants mock data loaded"
 echo "Loading Tables mock data"
-mongoimport --db eatSocialDev --collection tables --drop --host=127.0.0.1 --file tables.json
+mongoimport --db eatSocialDev --collection tables --drop --host=127.0.0.1 --file tables.json --jsonArray
 echo "Tables mock data loaded"
 echo "Loading menus mock data"
-mongoimport --db eatSocialDev --collection menus --drop --host=127.0.0.1 --file menus.json
+mongoimport --db eatSocialDev --collection menus --drop --host=127.0.0.1 --file menus.json --jsonArray
 echo "Menu mock data loaded"
 echo "Loading MenuItems mock data"
-mongoimport --db eatSocialDev --collection menu_items --drop --host=127.0.0.1 --file menuItems.json
+mongoimport --db eatSocialDev --collection menu_items --drop --host=127.0.0.1 --file menuItems.json --jsonArray
 echo "MenuItems mock data loaded"
